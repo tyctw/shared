60f0fb4b895c3d0a8f452ef2c55b8c8e5d6104c5
tyctw <tyctw.analyze@gmail.com>
2026-06-09 23:46:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 設定相對路徑，確保在 GitHub Pages 子目錄能正確運作
+  base: '/shared/',
   define: {
     // 避免 process is not defined 錯誤 (如果程式碼中有用到 process.env)
     'process.env': {} 
