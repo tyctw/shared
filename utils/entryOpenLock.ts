271e2e881e39d6e1f3e42131580212455f004cde
tyctw <tyctw.analyze@gmail.com>
2026-07-07 10:31:20 +0800

    Update entryOpenLock.ts

--- utils/entryOpenLock.ts
+++ utils/entryOpenLock.ts
@@ -1,6 +1,6 @@
 export const LOCKED_ENTRY_YEAR = 115;
-export const ENTRY_OPEN_AT_ISO = '2026-07-07T08:00:00+08:00';
-export const ENTRY_OPEN_AT_LABEL = '115/07/07 08:00';
+export const ENTRY_OPEN_AT_ISO = '2026-07-07T10:55:00+08:00';
+export const ENTRY_OPEN_AT_LABEL = '115/07/07 10:55';
 
 export const isEntryYearLocked = (year: number | string, now = Date.now()) => {
   return Number(year) === LOCKED_ENTRY_YEAR && now < Date.parse(ENTRY_OPEN_AT_ISO);
